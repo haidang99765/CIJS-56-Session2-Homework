c61f581b1a24ccc96bf43b1cf3140f53b1ae6535
DESKTOP-ALPS9AN\Itachi <hoanghaidang2407@gmail.com>
2021-01-23 12:03:55 +0700

    update

--- homework.js
+++ homework.js
@@ -1,3 +1,4 @@
+//bài 1
 class Meme {
     id;
     name;
@@ -23,3 +24,73 @@ console.log ('=============')
 meme2.show();
 console.log (meme1);
 console.log (meme2);
+
+//bài 2
+class hanghoa {
+    id;
+    name;
+    price;
+    nhasanxuat;
+    discount;
+    ngaynhap;
+    constructor (id, name, price, nhasanxuat, discount, ngaynhap) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.nhasanxuat = nhasanxuat;
+        this.ngaynhap = ngaynhap;
+    }
+}
+
+class DoGiaDung extends hanghoa {
+    doben;
+    constructor (id, name, price, nhasanxuat, discount, ngaynhap, doben) {
+        super(id, name, price, nhasanxuat, discount, ngaynhap);
+        this.doben = doben;
+    }
+    discount1() {
+        console.log ('Số tiền được giảm giá: ' + this.price * 0.1 + ' ' + 'đồng');
+    }
+}
+
+class QuanAo extends hanghoa {
+    xuatxu;
+    chatlieu;
+    constructor (id, name, price, nhasanxuat, discount, ngaynhap, xuatxu, chatlieu) {
+        super(id, name, price, nhasanxuat, discount, ngaynhap);
+        this.xuatxu = xuatxu;
+        this.chatlieu = chatlieu;
+    }
+    discount2() {
+        console.log ('Số tiền được giảm giá: ' + this.price * 0.05 + ' ' + 'đồng');
+    }
+}
+
+class Food extends hanghoa {
+    vi;
+    constructor (id, name, price, nhasanxuat, discount, ngaynhap, vi) {
+        super(id, name, price, nhasanxuat, discount, ngaynhap);
+        this.vi = vi;
+    }
+    discount3() {
+        console.log ('Số tiền được giảm giá: ' + this.price * 0.02 + ' ' + 'đồng');
+    }
+}
+
+var BimBim = new Food (1, 'Bimbim', 15000, 'Hảo Hảo', 2 ,  '24/02/2021', 'cay');
+console.log (BimBim);
+BimBim.discount3();
+
+var Noodle = new Food (2, 'Mì tôm Miliket', 7000, 'Hảo Hảo', 2 ,  '22/02/2021', 'chua');
+console.log (Noodle);
+Noodle.discount3();
+
+var Jogger = new QuanAo (3, 'Jogger', 100000, 'Chinatown', 5, '23/01/2021', 'Trung Quốc', 'Vải thô');
+console.log (Jogger);
+Jogger.discount2();
+
+var Pan = new DoGiaDung (4, 'Chảo', 2000000, 'Vietnam Coporation', 10, '23/01/2021', 'Không rỉ sét');
+console.log (Pan);
+Pan.discount1();
+
+

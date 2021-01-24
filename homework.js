f647b68da2b248861c0cdaa2979310771a8670e2
DESKTOP-ALPS9AN\Itachi <hoanghaidang2407@gmail.com>
2021-01-24 20:30:57 +0700

    update homework day 2

--- homework.js
+++ homework.js
@@ -11,21 +11,64 @@ class Meme {
         this.dateModified = dateModified;
     }
     show() {
-        console.log ('Tên của meme là: ' + this.name);
-        console.log ('Link ảnh: ' + this.image);
-        console.log ('Ngày tạo: ' + this.dateModified);
+        // console.log ('Tên của meme là: ' + this.name);
+        // console.log ('Link ảnh: ' + this.image);
+        // console.log ('Ngày tạo: ' + this.dateModified);
+        document.getElementById("name").innerText = this.name;
+        document.getElementById("image").src = this.image;
+        document.getElementById("dateModified").innerText = this.dateModified;
+    }
+    update(data) {
+        this.name = data.name;
+        this.image = data.image;
     }
 }
 
-var meme1 = new Meme('1', 'chúa hề', 'https://i.pinimg.com/originals/75/31/5d/75315db511a058432745fc37d82a7c44.png', '23/1/2021')
-var meme2 = new Meme('2', 'dog', 'https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2020/10/meme-hong-hot.jpg', '23/1/2021')
-meme1.show();
-console.log ('=============')
-meme2.show();
-console.log (meme1);
-console.log (meme2);
+class MemeCollection {
+    id;
+    name;
+    owner;
+    arrofmeme = [];
+    constructor(id, name, owner, arrofmeme) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.arrofmeme = arrofmeme;
+    }
 
-//bài 2
+    AddMeme(meme) { //thêm 1 meme
+        this.arrofmeme.push(meme);
+    }
+
+    UpdateMeme(id, data) { //thay đổi thông tin của meme có id tương ứng
+        for (var i = 0; i < this.arrofmeme.length; i++) {
+            if (this.arrofmeme[i].id == id) {
+                this.arrofmeme[i].update(data);
+            }
+        }
+    }
+
+    DeleteMeme(id) { //xóa 1 meme có id tương ứng
+        for (var i = 0; i < this.arrofmeme.length; i++) {
+            if (this.arrofmeme[i].id == id) {
+                this.arrofmeme.splice(i, 1);
+            }
+        }
+    }
+}
+var meme1 = new Meme(1, 'chúa hề', 'https://i.pinimg.com/originals/75/31/5d/75315db511a058432745fc37d82a7c44.png', '23/1/2021')
+meme1.show();
+var meme2 = new Meme(2, 'dog', 'https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2020/10/meme-hong-hot.jpg', '23/1/2021')
+var meme3 = new Meme(3, 'black', 'https://i.pinimg.com/236x/ba/71/d9/ba71d987802bd79b169478c3f69fcd77.jpg', '24/1/2021')
+var CollectionOfMeme = new MemeCollection (1, 'CollectionOfMeme', 'Dang', [meme1, meme2]);
+CollectionOfMeme.AddMeme(meme3);
+CollectionOfMeme.UpdateMeme(2, {name: "Test"});
+CollectionOfMeme.DeleteMeme(2);
+console.log (CollectionOfMeme);
+//bug: nếu xóa và update bất kì 1 meme có id nào thì link image của 1 meme khác trong mảng sẽ thành undefined, còn nếu xóa và update cùng 1 id thì 
+//link image của các meme khác vẫn còn, em chưa hiểu thế là bị gì :(
+
+//bài 2, em có làm thử theo suy nghĩ của em, chắc là sai nên có gì thứ 2 đi học anh chữa lại bài này nhá
 class hanghoa {
     id;
     name;
@@ -92,5 +135,10 @@ Jogger.discount2();
 var Pan = new DoGiaDung (4, 'Chảo', 2000000, 'Vietnam Coporation', 10, '23/01/2021', 'Không rỉ sét');
 console.log (Pan);
 Pan.discount1();
+
+
+
+
+    
 
 
